4fdf5017e7951dc0deffb10074710ea87f3f82bd
manuelpravia <praviapuri@gmail.com>
2024-02-08 01:54:58 -0500

    feat: Add config cloud

--- gateway_server/src/main/java/com/mpraviap/gateway_server/GatewayServerApplication.java
+++ gateway_server/src/main/java/com/mpraviap/gateway_server/GatewayServerApplication.java
@@ -1,15 +1,15 @@
-package com.mpraviap.sales_service;
+package com.mpraviap.gateway_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
-public class SalesServiceApplication {
+@SpringBootApplication
+public class GatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SalesServiceApplication.class, args);
+		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
 }
--- provider_service/src/main/java/com/mpraviap/provider_service/ProviderServiceApplication.java
+++ provider_service/src/main/java/com/mpraviap/provider_service/ProviderServiceApplication.java
@@ -2,7 +2,9 @@ package com.mpraviap.provider_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderServiceApplication {
 
